a401f8d46adbc520d36f8dbb58e276939a38aabd
tmquoc <tmquoc1299@gmail.com>
2023-11-03 10:57:09 +0700

    docker

--- setup-docker.sh
+++ setup-docker.sh
@@ -3,8 +3,11 @@
 # build the flask container
 docker build -t prakhar1989/foodtrucks-web .
 
+# create the network
+docker network create foodtrucks-net
+
 # start the ES container
-docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --name es docker.elastic.co/elasticsearch/elasticsearch:6.3.2
+docker run -d --name es --net foodtrucks-net -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
 
 # start the flask app container
-docker run -d -p 5000:5000 --name web prakhar1989/foodtrucks-web
+docker run -d --net foodtrucks-net -p 5000:5000 --name foodtrucks-web prakhar1989/foodtrucks-web
